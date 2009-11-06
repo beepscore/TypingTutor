e6a60d59a3a3d4f5264e9af4589ca0970a7fa9c7
beepscore <support@beepscore.com>
2009-11-05 21:18:56 -0800

    Setup .xib.  Ref Hillegass Ch 19 pg 266-269

---
+++ BigLetterView.h
@@ -0,0 +1,16 @@
+//
+//  BigLetterView.h
+//  TypingTutor
+//  Ref Hillegass Ch 19
+//  Created by Steve Baker on 11/5/09.
+//  Copyright 2009 Beepscore LLC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface BigLetterView : NSView {
+
+}
+
+@end
---
+++ BigLetterView.m
@@ -0,0 +1,26 @@
+//
+//  BigLetterView.m
+//  TypingTutor
+//
+//  Created by Steve Baker on 11/5/09.
+//  Copyright 2009 Beepscore LLC. All rights reserved.
+//
+
+#import "BigLetterView.h"
+
+
+@implementation BigLetterView
+
+- (id)initWithFrame:(NSRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect {
+    // Drawing code here.
+}
+
+@end
