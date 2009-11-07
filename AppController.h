e34af79d0be987b58ca466c43c954ebf2a3c1862
beepscore <support@beepscore.com>
2009-11-06 16:23:00 -0800

    Add AppController, but don't use yet. Ref Hillegass Ch 24 pg 310

---
+++ AppController.h
@@ -0,0 +1,20 @@
+//
+//  AppController.h
+//  TypingTutor
+//
+//  Created by Steve Baker on 11/6/09.
+//  Copyright 2009 Beepscore LLC. All rights reserved.
+//
+
+// Ref Hillegass pg 310
+#import <Cocoa/Cocoa.h>
+@class BigLetterView;
+
+
+@interface AppController : NSObject {
+#pragma mark Outlets
+    
+
+}
+
+@end
---
+++ AppController.m
@@ -0,0 +1,15 @@
+//
+//  AppController.m
+//  TypingTutor
+//
+//  Created by Steve Baker on 11/6/09.
+//  Copyright 2009 Beepscore LLC. All rights reserved.
+//
+
+#import "AppController.h"
+#import "BSGlobalValues.h"
+
+
+@implementation AppController
+
+@end
