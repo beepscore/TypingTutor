06098788868eb5072434c2800d654a15755a0622
beepscore <support@beepscore.com>
2009-11-08 10:54:43 -0800

    Start Hillegass Ch 25.

--- BigLetterView.h
+++ BigLetterView.h
@@ -1,7 +1,6 @@
 //
 //  BigLetterView.h
 //  TypingTutor
-//  Ref Hillegass Ch 19, 20, 21, 22, 23, 24
 //  Created by Steve Baker on 11/5/09.
 //  Copyright 2009 Beepscore LLC. All rights reserved.
 //
